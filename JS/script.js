8ed11609da55458b2aad7618608cc6e68012afba
Naveen Kumar <naveenkumarreddy967@gmail.com>
2022-05-25 15:01:29 +0530

    NavBar Final

--- JS/script.js
+++ JS/script.js
@@ -27,11 +27,17 @@ var isActive = false
 hamburger.addEventListener('click', function () {
   mobileNav.classList.toggle('open')
   if (!isActive) {
+    nav.style.backgroundColor = '#62466b'
+    nav.style.backdropFilter = 'blur(0px)'
+    nav.style.height = '100vh'
     bars[0].style.transform = 'rotate(45deg)'
     bars[1].style.opacity = '0'
     bars[2].style.transform = 'rotate(-45deg)'
     isActive = true
   } else {
+    nav.style.backgroundColor = 'transparent'
+    nav.style.backdropFilter = 'blur(10px)'
+    nav.style.height = 'auto'
     bars[0].style.transform = 'rotate(0deg)'
     bars[1].style.opacity = '1'
     bars[2].style.transform = 'rotate(0deg)'
